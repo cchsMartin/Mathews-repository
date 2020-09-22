4fc359912d359d834670a385e421bf953473062f
StephenMullori <70962083+StephenMullori@users.noreply.github.com>
2020-09-22 16:51:46 -0400

    Update NewEmailJava.java

--- NewEmailJava.java
+++ NewEmailJava.java
@@ -7,32 +7,30 @@
 
 import java.util.Scanner;
 
-import java.util.Random;
-
 public class NewEmailJava {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("What is your first name");
+		System.out.println("What is your first name");// creates first name and inital
 		String firstName = input.nextLine();
 		firstName = firstName.toLowerCase();
 		
 		String firstInital = firstName.toUpperCase().substring(0,1);
 		
 		
-		System.out.println("What is your second name");
+		System.out.println("What is your second name");// creates middle name and inital
 		String middleName = input.nextLine();
 		middleName = middleName.toLowerCase();
 		
 		String middleInital = middleName.toUpperCase().substring(0,1);
 		
-		System.out.println("What is your last name");
+		System.out.println("What is your last name");// creates last name
 		String lastName = input.nextLine();
 		lastName = lastName.toLowerCase();
 		
 		
-		System.out.println("What is your date of birth dd-mm-yyyy");
+		System.out.println("What is your date of birth dd-mm-yyyy");//creates date of birth and password
 		String dob = input.nextLine();
 		
 		String password = dob.substring(0,2) + dob.substring(3,5) + dob.substring(6,10);
